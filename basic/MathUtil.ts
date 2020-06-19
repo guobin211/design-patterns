ff843bedb9801131bfee1299c16dcd59d7e7ccec
GuoBin <guobin201314@gmail.com>
2020-06-19 16:46:28 +0800

    feature(ts): basic

---
+++ basic/MathUtil.ts
@@ -0,0 +1,25 @@
+/**
+ * MathUtil.ts
+ * @Date 2020-06-19
+ * @Author GuoBin<guobin201314@gmail.com>
+ * @Project design-patterns
+ */
+console.log(Math);
+
+console.log(
+  Math.E,
+  Math.LN2,
+  Math.LN10,
+  Math.LOG2E,
+  Math.LOG10E,
+  Math.PI,
+  Math.SQRT1_2,
+  Math.SQRT2,
+);
+console.log(
+  Math.abs(-12),
+  Math.acos(0.5),
+  Math.acosh(1),
+  Math.asin(0.5),
+  Math.asinh(1),
+);
---
+++ basic/TypedArray.ts
@@ -0,0 +1,24 @@
+/**
+ * TypedArray.ts
+ * @Date 2020-06-19
+ * @Author GuoBin<guobin201314@gmail.com>
+ * @Project design-patterns
+ */
+class Person {
+  constructor(public name: string, public age: number) {}
+}
+// 带空间的数组不能直接push
+const persons: Person[] = new Array(10);
+for (let i = 0; i < 10; i++) {
+  persons[i] = new Person(`第${i}个`, parseInt(Math.random() * 100 + "", 10));
+}
+console.log(persons);
+
+const add = new Person("新加", 22);
+persons.push(add);
+console.log(persons);
+console.log(persons.length);
+
+const first = persons.shift();
+const end = persons.pop();
+console.log(first, end, persons);
