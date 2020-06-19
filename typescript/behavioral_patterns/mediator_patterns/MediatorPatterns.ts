2da48b1a99208c1603cc8006db957e203c2e258d
GuoBin <guobin201314@gmail.com>
2020-06-19 14:32:21 +0800

    feature(ts): 中介者模式

---
+++ typescript/behavioral_patterns/mediator_patterns/MediatorPatterns.ts
@@ -0,0 +1,34 @@
+/**
+ * MediatorPatterns.ts
+ * @Date 2020-06-19
+ * @Author GuoBin<guobin201314@gmail.com>
+ * @Project design-patterns
+ */
+abstract class BaseMediator {
+  abstract register(c: BaseClient): void;
+  abstract publish(s: string): void;
+}
+
+abstract class BaseClient {
+  abstract sendMsg(p: string): void;
+}
+
+class WebClient extends BaseClient {
+  sendMsg(p: string) {
+    console.log(`WebClient send ${p}`);
+  }
+}
+
+class MediatorPatterns extends BaseMediator {
+  private _clients: BaseClient = new WebClient();
+  publish(s: string): void {
+    console.log(`MediatorPatterns: call publish`);
+    this._clients.sendMsg(s);
+  }
+
+  register(c: BaseClient): void {
+    this._clients = c;
+  }
+}
+
+new MediatorPatterns().publish("Hello");
