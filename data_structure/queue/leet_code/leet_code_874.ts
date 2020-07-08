5820577de6e753d88c477e91bb34997b5c1d0292
GuoBin <guobin201314@gmail.com>
2020-07-08 10:38:19 +0800

    feature(data): 完全平方数

--- data_structure/queue/leet_code/leet_code_874.ts
+++ data_structure/queue/leet_code/leet_code_874.ts
@@ -7,24 +7,25 @@
 
 // 输入: n = 13
 // 输出: 2
-// 解释: 13 = 4 + 9
+// 解释: 13 = 4 + 9  完全平方数
 function numSquares(n: number): number {
-  let count = 0;
-  if (Math.sqrt(n) % 1 === 0) {
-    count++;
-    return count;
-  } else {
-    for (let i = 1; i < n / 2; i++) {
-      if (Math.sqrt(n) % 1 === 0 && Math.sqrt(n - 1) % 1 === 0) {
-        count++;
-        return count;
+  const dp = new Array(n + 1).fill(Number.MAX_SAFE_INTEGER);
+  dp[0] = 0;
+  let max_square_index = parseInt((Math.sqrt(n) + 1).toString());
+  let square_nums = new Array(max_square_index);
+  for (let i = 0; i < max_square_index; i++) {
+    square_nums[i] = i * i;
+  }
+  for (let i = 0; i <= n ; i++) {
+    for (let j = 1; j < max_square_index; j++) {
+      if (i < square_nums[j]) {
+        break;
       }
+      dp[i] = Math.min(dp[i], dp[i - square_nums[j]] + 1);
     }
   }
-
-  function isInt(n: number) {
-    return !isNaN(n) && n % 1 === 0;
-  }
-
-  return count;
+  return dp[n];
 }
+
+console.log(numSquares(12));
+console.log(numSquares(13));
