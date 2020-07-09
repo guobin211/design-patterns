a5ae96e8ea491043580860e51ff9fa311a34e43c
GuoBin <guobin201314@gmail.com>
2020-07-09 14:45:23 +0800

    style(shell): format shell

--- deno.sh
+++ deno.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-project_path=$(cd `dirname $0`; pwd)
+project_path=$(cd "$(dirname "$0")" || exit ; pwd)
 project_name="${project_path##*/}"
 
-echo $project_path
-echo $project_name
+echo "$project_path"
+echo "$project_name"
 
-read -p "请输入文件路径" file_path
-echo $file_path
+read -r -p "请输入文件路径: " file_path
+
+printf '你输入的文件名是: \t%s\n' "$file_path"
