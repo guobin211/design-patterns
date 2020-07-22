8a1bf66a744df23b272d7ab60fe708f879a90fc2
GuoBin <guobin201314@gmail.com>
2020-07-22 15:30:18 +0800

    code(leet_code): 递归相关算法

--- data_structure/linked_list/leet_code/leet_code_1227.ts
+++ data_structure/linked_list/leet_code/leet_code_1227.ts
@@ -1,1 +1,18 @@
+// 实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+function myPow(x: number, n: number): number {
+  let N = n;
+  return N > 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
 
+  function quickMul(x1: number, n1: number): number {
+    let answer = 1.0;
+    let start = x1;
+    while (n1 > 0) {
+      if (n1 % 2 === 1) {
+        answer *= start;
+      }
+      start *= start;
+      n1 /= 2;
+    }
+    return answer;
+  }
+}
--- data_structure/tree/leet_code/leet_code_1225.ts
+++ data_structure/tree/leet_code/leet_code_1225.ts
@@ -1,1 +1,30 @@
+// 给定一个二叉树，找出其最大深度。
+//
+// 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+//
+// 说明: 叶子节点是指没有子节点的节点。
 
+class TreeNode {
+  val: number | undefined;
+  left: TreeNode | null;
+  right: TreeNode | null;
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val;
+    this.left = (left === undefined ? null : left);
+    this.right = (right === undefined ? null : right);
+  }
+}
+
+function maxDepth(root: TreeNode | null): number {
+  return root ? Math.max(maxDepth(root!.left), maxDepth(root!.right)) + 1 : 0;
+  // if (root === null) {
+  //   return 0;
+  // } else {
+  //   if (root.left === null && !root.right === null) {
+  //     return 1;
+  //   } else {
+  //     return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+  //   }
+  // }
+}
