a912765ed3c171063a1c2df2caf06f59df0cb9ec
GuoBin <guobin201314@gmail.com>
2020-07-28 17:57:30 +0800

    feat(leet): 合并链表

---
+++ data_structure/linked_list/leet_code/leet_code_762.ts
@@ -0,0 +1,40 @@
+// 合并两个有序链表
+// 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+//
+//
+//
+// 示例：
+//
+// 输入：1->2->4, 1->3->4
+// 输出：1->1->2->3->4->4
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+  }
+}
+
+function mergeTwoLists(
+  l1: ListNode | null,
+  l2: ListNode | null,
+): ListNode | null {
+  const dummy = new ListNode();
+  let current = dummy;
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      current.next = l1;
+      l1 = l1.next;
+    } else {
+      current.next = l2;
+      l2 = l2.next;
+    }
+    current = current.next;
+  }
+  // 填充剩下的
+  current.next = l1 ? l1 : l2;
+  return dummy.next;
+}
